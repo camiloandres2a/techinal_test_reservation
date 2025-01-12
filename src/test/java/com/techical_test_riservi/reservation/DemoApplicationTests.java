b773806d272d4822fc1f97a8fcfa85b7f108dd68
camiloandres2a <caalvarez2401@gmail.com>
2025-01-11 19:04:35 -0500

    Creando proyecto Reservacion

---
+++ src/test/java/com/techical_test_riservi/reservation/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techical_test_riservi.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
